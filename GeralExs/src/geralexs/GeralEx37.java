dd4636ba61a138e83722c0a51bceec59388ee21a
gabriel <71945882+Hikaruya9@users.noreply.github.com>
2024-09-10 19:53:51 -0300

    Geral update

--- GeralExs/src/geralexs/GeralEx37.java
+++ GeralExs/src/geralexs/GeralEx37.java
@@ -24,18 +24,34 @@ public class GeralEx37 {
         System.out.println("");
     }
     
-    public static void mostCommonElement(ArrayList<Integer> list) {
-        
+    public static int verifyMostCommonElement(ArrayList<Integer> list) {
+        int countV = 0, mostCommonElement = 0;
+        for (int i = 0; i <= list.size(); i++) {
+            for (int j = i+1; j < list.size(); j++) {
+                int countT = 0;
+                if (list.get(i) == list.get(j)) {
+                    countT++;
+                    if (countT > countV) {
+                        mostCommonElement = list.get(i);
+                        countV = countT;
+                    }
+                }
+            }
+        }
+        System.out.println(countV);
+        return mostCommonElement;
     }
     
     public static void main(String[] args) {
         ArrayList<Integer> lista = new ArrayList<>();
-        int maxL = 10,maxV = 10;
+        int maxL = 10,maxV = 5;
         
         arrayListRandom(lista,maxL,maxV);
         System.out.println("Original list:");
         arrayListShow(lista);
+        System.out.println("------------------------");
         
-        
+        System.out.println("Elemento mais comum da lista:\n"+verifyMostCommonElement(lista));
+        System.out.println();
     }
 }
